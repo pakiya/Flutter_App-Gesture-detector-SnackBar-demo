81f4906bac94183a936b05a5a59b04e6aec5f2c5
Pankaj kumar <pk.kumar7500@gmail.com>
2019-05-20 16:17:24 +0530

    first commit for gesture detector demo

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import './ui/home.dart';
+
+void main() {
+  var title = "Gestures";
+  runApp(new MaterialApp(
+    title: title,
+    home: new Home(
+      title: title,
+    ),
+  ));
+}
